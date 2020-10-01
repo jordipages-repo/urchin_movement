38f82d344cf80fa1b1f0d779bdd2535371f9491a
Jordi Pagès i Fauria <jordi_pages@cc76.ceab.csic.es>
2020-10-01 15:54:31 +0200

    Trajectory visualisations and entropy estimation

--- FinalGGPLOTs.R
+++ FinalGGPLOTs.R
@@ -1,16 +1,17 @@
+# Running other scripts before plotting:
+source("urchins_tortuosity_classic.R")
+rm(list = setdiff(ls(), c("tort", "speed")))
+detach("package:dplyr")
+source("urchins_q_moments.R")
+rm(list = setdiff(ls(), c("tort", "speed", "fin", "listExp.urch.null", "listExp.urch.pred")))
+
+# Running script with some of the functions needed
+source("qmomentsFunctions.R")
+
 library(tidyverse)
 library(dplyr)
 library(ggsci)
 library(cowplot)
-
-# Running other scripts before plotting:
-source("urchins_tortuosity_classic.R")
-rm(list = setdiff(ls(), c("tort", "speed")))
-source("urchins_q_moments.R")
-rm(list = setdiff(ls(), c("tort", "speed", "fin")))
-
-# Running script with some of the functions needed
-source("qmomentsFunctions.R")
 
 # Adding labels to each data set
 fin$labels <- ifelse(fin$treatment== "Control", "a", "b")
@@ -70,6 +71,65 @@ box.exponents <- ggplot(fin, aes(x = treatment, y = coef)) +
 plot_grid(box.straightness, box.velocity, box.exponents, ncol = 1, align = 'v', labels = "AUTO")
 # ggsave2("Figs/Cowplot_boxplots.pdf")
 # ggsave2("Figs/Cowplot_boxplots.pdf", width = 130, height = 300, units = "mm")
+
+
+# # # 
+# VIOLINplots. Comparing all response variables ----
+# # # 
+violin.straightness <- ggplot(tort, aes(x = exp, y = tortuosity)) +
+  geom_violin(aes(fill = exp)) +
+  geom_boxplot(width=0.05) +
+  # stat_summary(fun=median, geom="point", size=3, color="black", pch = 15) +
+  geom_text(aes(y = max(tortuosity) + 0.1, label = labels), check_overlap = T) +
+  scale_fill_d3(palette = "category20") +
+  scale_y_continuous(breaks = c(0,0.25,0.5, 0.75,1), labels = c("0.00","0.25","0.50","0.75", "1.00")) +
+  coord_cartesian(ylim = c(0,1.1)) +
+  # scale_y_continuous(labels = scales::number_format(accuracy = 0.1)) +
+  xlab("") +
+  ylab("Straightness index") +
+  theme_bw() +
+  theme(legend.position = "none", 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text = element_text(size = 18))
+
+violin.velocity <- ggplot(speed, aes(x = exp, y = mean.speed.cm)) +
+  geom_violin(aes(fill = exp)) +
+  geom_boxplot(width=0.05) +
+  # stat_summary(fun=median, geom="point", size=3, color="black", pch = 15) +
+  geom_text(aes(y = max(mean.speed.cm) + 1, label = labels), check_overlap = T) +
+  scale_fill_d3(palette = "category20") +
+  # scale_y_continuous(labels = scales::number_format(accuracy = 1)) +
+  xlab("") +
+  ylab(expression(paste("Mean speed (cm minute"^"-1",")"))) +
+  theme_bw() +
+  theme(legend.position = "none", 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text = element_text(size = 18))
+
+violin.exponents <- ggplot(fin, aes(x = treatment, y = coef)) +
+  geom_violin(aes(fill = treatment)) +
+  geom_boxplot(width=0.05) +
+  # stat_summary(fun=median, geom="point", size=3, color="black", pch = 15) +
+  geom_text(aes(y = max(coef) + 0.1, label = labels), check_overlap = T) +
+  scale_fill_d3(palette = "category20") +
+  geom_hline(yintercept = 0.5, lty = 2) +
+  geom_hline(yintercept = 1, lty = 3) +
+  scale_y_continuous(breaks = c(0,0.25,0.5, 0.75,1), labels = c("0.00","0.25","0.50","0.75", "1.00")) +
+  # scale_y_continuous(labels = scales::number_format(accuracy = 0.1)) +
+  xlab("") +
+  ylab(expression(paste(zeta,"(q)"))) +
+  coord_cartesian(ylim = c(0,1.1)) +
+  theme_bw() +
+  theme(legend.position = "none", 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text = element_text(size = 18))
+
+plot_grid(violin.straightness, violin.velocity, violin.exponents, ncol = 1, align = 'v', labels = "AUTO")
+# ggsave2("Figs/Cowplot_violinplots.pdf")
+ggsave2("Figs/Cowplot_violinplots.pdf", width = 130, height = 300, units = "mm")
 
 
 # # # 
@@ -147,8 +207,8 @@ plot_grid(bar.straightness, bar.velocity, bar.exponents, ncol = 1, align = 'v', 
 # # # 
 # GGPLOTTING INDIVIDUAL EXPONENTS URCH.NULL ----
 # # # 
-
 library(tidyverse)
+qs <- seq(from = 0, to = 8, by=1)
 as_tibble(listExp.urch.null) %>% 
   mutate(qs = rep(qs, length(unique(listExp.urch.null$ID)))) %>% 
   ggplot() + 
@@ -188,3 +248,13 @@ as_tibble(listExp.urch.pred) %>%
         panel.grid.minor = element_blank(),
         text = element_text(size = 18))
 # ggsave2(filename = "Figs/indiv.exponents.predators.pdf")
+
+
+# # # 
+# GGPLOTTING Qmoment HISTOGRAMS
+# # #
+
+ggplot(data = fin) +
+  geom_histogram(aes(coef, fill = treatment), binwidth = 1)
+  
+
